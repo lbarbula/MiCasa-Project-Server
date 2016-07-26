e0c4cf6c12ad030efed9b9c02acfd1dc0dc8ed69
Will Thomas <wlthomas001@gmail.com>
2016-07-26 17:33:21 -0600

    add cors and basic get

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('../db/api');
+var db = require('../db/api');
 var path = require('path');
 
 /* GET home page. */
@@ -12,7 +12,10 @@ var path = require('path');
 // });
 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views', 'index.html'));
+  db.findAllBusinesses().then(function(data) {
+  console.log(data);
+  res.json(data)
+});
 });
 
 
