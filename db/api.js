d51e29bef98458a9835f14c14652fa471ab316bc
Will Thomas <wlthomas001@gmail.com>
2016-07-30 14:15:40 -0600

    add query for business by id

--- db/api.js
+++ db/api.js
@@ -22,5 +22,20 @@ module.exports = {
     .join('owner', function() {
       this.on('business_owner.owner_id', '=', 'owner.id')
     })
+  },
+  findBusinessesAndOwnersById: function (id) {
+    return knex('business')
+    .select()
+    .join('industry', function() {
+      this.on('business.industry_id', '=', 'industry.id')
+    })
+    .join('business_owner', function() {
+      this.on('business.id', '=', 'business_owner.business_id')
+    })
+    .join('owner', function() {
+      this.on('business_owner.owner_id', '=', 'owner.id')
+    })
+    .where('business.id', '=', id);
+
   }
 };
