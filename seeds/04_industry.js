b1ef838d2f6b3c62ffa045574ceb8d4358db6ae9
Will Thomas <wlthomas001@gmail.com>
2016-07-30 11:01:33 -0600

    change industry migration from name to type to make it easier to join business and industry

--- seeds/04_industry.js
+++ seeds/04_industry.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise) {
 	return Promise.all([
-		knex('industry').insert({name: 'Agriculture'}),
-		knex('industry').insert({name: 'Construction'})
+		knex('industry').insert({type: 'Agriculture'}),
+		knex('industry').insert({type: 'Construction'})
 	]);
 };
