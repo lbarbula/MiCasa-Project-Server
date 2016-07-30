05e0daba91ef82bd8882e809c6cf0d0e7c94133c
Will Thomas <wlthomas001@gmail.com>
2016-07-30 10:29:08 -0600

    seperate routes for entrepreneurs and businesses

---
+++ routes/businesses.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db/api')
+
+
+router.get('/', function(req, res, next) {
+  db.findAllBusinesses().then(function(data) {
+  console.log(data);
+  res.json(data)
+});
+});
+
+module.exports = router;
