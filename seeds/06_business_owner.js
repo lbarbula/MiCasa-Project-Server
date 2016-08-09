7fc073e5f0d4c94eb9911a433eed54ffacdeae0f
Will Thomas <wlthomas001@gmail.com>
2016-08-09 13:14:46 -0600

    add new seeds

--- seeds/06_business_owner.js
+++ seeds/06_business_owner.js
@@ -15,6 +15,30 @@ exports.seed = function(knex, Promise) {
 			knex('business_owner').insert({
 				owner_id: find.findFromList('Alex', owner, "first_name"),
 				business_id: find.findFromList('Bonkers', business, "name")
+			}),
+			knex('business_owner').insert({
+				owner_id: find.findFromList('Laurence', owner, "first_name"),
+				business_id: find.findFromList('CashMoney', business, "name")
+			}),
+			knex('business_owner').insert({
+				owner_id: find.findFromList('Piggot', owner, "first_name"),
+				business_id: find.findFromList('School Clubz', business, "name")
+			}),
+			knex('business_owner').insert({
+				owner_id: find.findFromList('Jeremy', owner, "first_name"),
+				business_id: find.findFromList('Sick Clothes For Animals', business, "name")
+			}),
+			knex('business_owner').insert({
+				owner_id: find.findFromList('Ron', owner, "first_name"),
+				business_id: find.findFromList('Wonder Tech', business, "name")
+			}),
+			knex('business_owner').insert({
+				owner_id: find.findFromList('Samantha', owner, "first_name"),
+				business_id: find.findFromList('Amazing Marketing Magic', business, "name")
+			}),
+			knex('business_owner').insert({
+				owner_id: find.findFromList('Sizzle', owner, "first_name"),
+				business_id: find.findFromList('GardensAndMore', business, "name")
 			})
 		]);
 	});
