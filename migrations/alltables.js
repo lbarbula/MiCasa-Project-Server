07165b79a96f14f51715374edaaeaac412935c81
Laney Smith <lsmith.unc@gmail.com>
2016-08-10 14:21:53 -0600

    Add update business routes

--- migrations/alltables.js
+++ migrations/alltables.js
@@ -23,6 +23,7 @@ exports.up = function(knex, Promise) {
       table.string('last_name');
       table.string('email');
       table.string('phone');
+      table.string('gender');
     });
   }).then(function(){
     return knex.schema.createTable('business', function(table) {
--- routes/businesses.js
+++ routes/businesses.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/api')
 
-
 router.get('/', function(req, res, next) {
 	db.findBusinessesAndOwners().then(function(data) {
 		res.json(data)
@@ -21,9 +20,7 @@ router.get('/industries', function(req, res, next) {
 	});
 });
 
-
 router.get('/:id', function(req, res, next) {
-	console.log('hi');
 	return Promise.all([
 			db.getCommentsById(req.params.id),
 			db.findBusinessesAndOwnersById(req.params.id)
@@ -33,16 +30,12 @@ router.get('/:id', function(req, res, next) {
 		})
 })
 
-// router.put('/:id/edit', function(req, res, next) {
-//   db.editBusiness(req.body).then(function() {
-//     res.json({message: "Business Updated"})
-//   })
-//
-// });
-/*
-
-There is something going wrong with the business & owner lookup when the seeds are rerun, when clicking on Bonkers it was bringing up the General Duffy's Hot Sauce Emporium detail and visa versa.
-
-*/
+router.put('/:id', function(req, res, next) {
+	db.updateBusiness(req.body).then(function() {
+		res.json({
+			message: "Business Updated"
+		})
+	})
+})
 
 module.exports = router;
