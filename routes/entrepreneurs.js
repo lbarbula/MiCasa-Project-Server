f2ab83ddeeb0b9d8f53170c74d2cc31f50dba3c9
Will Thomas <wlthomas001@gmail.com>
2016-09-16 15:32:15 -0600

    add routes to handle data request for entrepreneur page

--- routes/entrepreneurs.js
+++ routes/entrepreneurs.js
@@ -5,15 +5,24 @@ var db = require('../db/api')
 
 router.get('/', function(req, res, next) {
     db.findAllOwners().then(function(data) {
-    console.log(data);
     res.json(data)
   });
 });
+
+router.get('/:id', function(req, res, next) {
+  return Promise.all([
+    db.findEntrepreneurById(req.params.id),
+    db.findClassesById(req.params.id),
+    db.findBusinessesAndOwnersById(req.params.id)
+  ])
+  .then(function(data) {
+    res.json(data);
+  });
+});
+
 router.post('/add', function(req, res, next){
-  console.log("req.body", req.body);
   db.addOwner(req.body)
   .then(function(data){
-    console.log(data)
     res.json(data)
   })
 })
