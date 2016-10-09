14702c85906b6498d620c57390bfb216c0d49191
Lucas Barbula <lucas.barbula@gmail.com>
2016-10-09 13:01:20 -0600

    setting up database migrations

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db/api');
-var path = require('path');
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   db.findAllBusinesses().then(function(data) {
-//     console.log(data);
-//     res.render('index', { businesses: data });
-//   })
-// });
-
-// router.get('/', function(req, res, next) {
-//   db.findAllBusinesses().then(function(data) {
-//   console.log(data);
-//   res.json(data)
-// });
-// });
-
-
-var businesses = require('./businesses');
-router.use('/businesses', businesses);
-
-var entrepreneurs = require('./entrepreneurs');
-router.use('/entrepreneurs', entrepreneurs);
-
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
